a02eb3d2b3b0515692166ab35696ea0210219a5c
bl0m1 <hugo@blommail.com>
2020-08-12 15:19:55 +0000

    bind rtorrent to wg0 and remove unused files

--- root/root/startup-rtorrent.sh
+++ root/root/startup-rtorrent.sh
@@ -5,5 +5,8 @@ set -x
 # remove old session lock
 rm -f /config/session/rtorrent.lock
 
+# fetch ip for interface wg9, used to bind rtorrent to VPN
+IP=$(ip addr show wg0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+
 # run rtorrent
-rtorrent -n -o import=/config/.rtorrent.rc -o port_range=$BINDPORT-$BINDPORT
+rtorrent -n -o import=/config/.rtorrent.rc -o port_range=$BINDPORT-$BINDPORT -b $IP
